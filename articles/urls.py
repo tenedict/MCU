1ac854645c5d81c17314dacf41acbfe902427f1c
9776mk <9776mk@gmail.com>
2022-10-21 16:10:36 +0900

    캐러셀 추가

--- articles/urls.py
+++ articles/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("update/<int:pk>", views.update, name='update'),
     # path('<int:pk>/comments/', views.comment_create, name='comment_create'),
     path('<int:pk>/comments/<int:comment_pk>', views.comment_delete, name='comment_delete'),
+    path('intro/', views.intro, name="intro"),
 ]
