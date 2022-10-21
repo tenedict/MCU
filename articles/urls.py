c9b5359a7eae42d998734ca0149a9804e245f215
mhd329 <mhd329@gmail.com>
2022-10-21 11:37:08 +0900

    detail, delete 구현

---
+++ articles/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "articles"
+
+urlpatterns = [
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
+]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def detail(request, pk):
+    article = Article.objects.get(id=pk)
+    context = {
+        "article": article,
+    }
+    return render(request, "articles/detail.html", context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(id=pk)
+    article.delete()
+    return redirect("articles:index")
