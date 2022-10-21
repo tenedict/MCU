9adda4e2624a98a9f18d3372d573750ad85cb91f
mhd329 <mhd329@gmail.com>
2022-10-21 12:19:16 +0900

    detail, delete 구현 및 충돌 수정

--- articles/views.py
+++ articles/views.py
@@ -5,21 +5,32 @@ from .forms import ReviewForm
 # Create your views here.
 def index(request):
     reviews = Review.objects.all()
-    context = {
-        'reviews': reviews
-    }
-    return render(request, 'articles/index.html', context)
+    context = {"reviews": reviews}
+    return render(request, "articles/index.html", context)
+
 
 def create(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         # DB에 저장하는 로직
         review_form = ReviewForm(request.POST)
         if review_form.is_valid():
             review_form.save()
-            return redirect('articles:index')
-    else: 
+            return redirect("articles:index")
+    else:
         review_form = ReviewForm()
+    context = {"review_form": review_form}
+    return render(request, "articles/create.html", context=context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(id=pk)
     context = {
-        'review_form': review_form
+        "article": article,
     }
-    return render(request, 'articles/create.html', context=context)
+    return render(request, "articles/detail.html", context)
+
+
+def delete(request, pk):
+    article = Article.objects.get(id=pk)
+    article.delete()
+    return redirect("articles:index")
